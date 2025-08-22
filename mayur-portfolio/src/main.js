a80d4815716d79dd437a30e6d8afc8a2bcdb5aca
mbagul <mb632753@wne.edu>
2025-08-22 00:01:11 -0400

    navbar

--- mayur-portfolio/src/main.js
+++ mayur-portfolio/src/main.js
@@ -1,1 +1,7 @@
-//
+function toggleSlide() {
+  const el = document.getElementById("scrollText");
+  el.classList.toggle("-translate-x-full");
+  el.classList.toggle("translate-x-0");
+  el.classList.toggle("opacity-0");
+  el.classList.toggle("opacity-100");
+}
